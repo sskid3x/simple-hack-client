70b67eb2553f973a904dcd88b48b5e74270eec2f
sskid3x <skibidigyattrizzskibiditoilet@gmail.com>
2025-12-18 04:41:32 +0300

    added comments (//'s) to understand the code for you num numbs

--- src/main/java/com/example/liquorclient/Keybinds.java
+++ src/main/java/com/example/liquorclient/Keybinds.java
@@ -3,6 +3,7 @@ package com.example.liquorclient;
 import com.example.liquorclient.autokillaura.AutoAttackMod;
 import com.example.liquorclient.scaffold.ScaffoldMod;
 import com.example.liquorclient.aimassist.AimAssistMod;
+import com.example.liquorclient.gui.ModMenuScreen;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.option.KeyBinding;
@@ -10,19 +11,29 @@ import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybinds {
+    private static KeyBinding openMenuKey;
     private static KeyBinding autoAttackKey;
     private static KeyBinding scaffoldKey;
-    private static KeyBinding aimAssistKey;
-    private static KeyBinding espKey;
+    private static KeyBinding aimbotKey;
 
     public static void register() {
+        // GUI menu (Right Shift)
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Open hax",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "Liquor Client"
+        ));
+
+        // Kill Aura (R)
         autoAttackKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "Killaura",
+                "Kill Aura",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_R,
                 "Liquor Client"
         ));
 
+        // Scaffold (C)
         scaffoldKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "Scaffold",
                 InputUtil.Type.KEYSYM,
@@ -30,17 +41,20 @@ public class Keybinds {
                 "Liquor Client"
         ));
 
-        aimAssistKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "AimBot",
+        // Aimbot (X)
+        aimbotKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Aimbot",
                 InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_X,
+                GLFW.GLFW_KEY_Z,
                 "Liquor Client"
         ));
 
+        // Event loop
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openMenuKey.wasPressed()) client.setScreen(new ModMenuScreen());
             while (autoAttackKey.wasPressed()) AutoAttackMod.toggle();
             while (scaffoldKey.wasPressed()) ScaffoldMod.toggle();
-            while (aimAssistKey.wasPressed()) AimAssistMod.toggle();
+            while (aimbotKey.wasPressed()) AimAssistMod.toggle();
         });
     }
 }
